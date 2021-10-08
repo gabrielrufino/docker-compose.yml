140f1b44f2913c78db74120711d3188b4b7fb039
Gabriel Rufino <contato@gabrielrufino.com>
2021-10-08 20:00:31 -0300

    Testing error

--- tests/smoke/mautic.sh
+++ tests/smoke/mautic.sh
@@ -1,5 +1,5 @@
 echo "[Smoke] - Mautic"
 
 # docker-compose up -d mautic
-curl --connect-timeout 30 -I http://localhost:8082
+curl --connect-timeout 30 --fail -I http://localhost:8082
 docker-compose down
