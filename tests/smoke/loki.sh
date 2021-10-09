b52a5b855e46db9af8d0d28dec43cbe72cccf1c7
Gabriel Rufino <contato@gabrielrufino.com>
2021-10-09 11:12:05 -0300

    Update smoke tests

--- tests/smoke/loki.sh
+++ tests/smoke/loki.sh
@@ -1,4 +1,4 @@
-echo "[Smoke] - Loki"
+echo "[Smoke] - Loki\n"
 
 docker-compose up -d loki
 sleep 60
@@ -6,7 +6,10 @@ sleep 60
 HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:3100/ready)
 if [ $HTTP_STATUS -ne 200  ];
   then
+    docker-compose down
+    echo "[Fail]"
     exit 1
 fi
 
 docker-compose down
+echo "[Pass]"
--- tests/smoke/mautic.sh
+++ tests/smoke/mautic.sh
@@ -1,4 +1,4 @@
-echo "[Smoke] - Mautic"
+echo "[Smoke] - Mautic\n"
 
 docker-compose up -d mautic
 sleep 30
@@ -6,7 +6,10 @@ sleep 30
 HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8082)
 if [ $HTTP_STATUS -ne 302  ];
   then
+    docker-compose down
+    echo "[Fail]"
     exit 1
 fi
 
 docker-compose down
+echo "[Pass]"
