281978807657d44da6e3368552f16863575e8e60
Gabriel Rufino <contato@gabrielrufino.com>
2021-10-09 19:03:29 -0300

    Update strapi test

--- tests/smoke/strapi.sh
+++ tests/smoke/strapi.sh
@@ -6,6 +6,7 @@ sleep 60
 HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:1337)
 if [ $HTTP_STATUS -ne 200  ];
   then
+    echo "HTTP_STATUS=$HTTP_STATUS"
     docker-compose down
     echo "[Fail]"
     exit 1
