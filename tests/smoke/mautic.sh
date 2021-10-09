cca8c038913dbb63f4d36f15088d1c4b5570459b
Gabriel Rufino <contato@gabrielrufino.com>
2021-10-09 11:20:58 -0300

    Update mautic test

--- tests/smoke/mautic.sh
+++ tests/smoke/mautic.sh
@@ -3,8 +3,8 @@ echo "[Smoke] - Mautic\n"
 docker-compose up -d mautic
 sleep 30
 
-HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8082)
-if [ $HTTP_STATUS -ne 302  ];
+HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8082/installer)
+if [ $HTTP_STATUS -ne 200  ];
   then
     docker-compose down
     echo "[Fail]"
