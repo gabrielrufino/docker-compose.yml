db893a1f0da27df52d17a3b67e5d5cbbc6392d52
Gabriel Rufino <contato@gabrielrufino.com>
2021-10-09 16:32:20 -0300

    Smoke test for strapi

--- tests/smoke/strapi.sh
+++ tests/smoke/strapi.sh
@@ -1,7 +1,7 @@
 echo "[Smoke] - Strapi\n"
 
 docker-compose up -d strapi
-sleep 30
+sleep 60
 
 HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:1337)
 if [ $HTTP_STATUS -ne 200  ];
