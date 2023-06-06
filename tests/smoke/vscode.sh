57608f4d8587c803ecad5a627f0b81fbf94f09ac
Gabriel Rufino <contato@gabrielrufino.com>
2023-06-06 07:01:58 -0300

    Fixing vscode test

--- tests/smoke/vscode.sh
+++ tests/smoke/vscode.sh
@@ -4,8 +4,8 @@ docker compose up -d vscode
 
 wait-on http://localhost:3003 --timeout 60000
 
-HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:3003)
-if [ $HTTP_STATUS -ne 200  ];
+HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:3003?tkn=root)
+if [ $HTTP_STATUS -ne 302  ];
   then
     docker compose down
     echo "[Fail]"
