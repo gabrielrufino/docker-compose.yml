12a157beb1bcc161029fa278d494b56a33494865
Gabriel Rufino <contato@gabrielrufino.com>
2026-07-20 20:17:52 -0300

    test(postgres): fail fast if wait-on times out

--- tests/smoke/postgres.sh
+++ tests/smoke/postgres.sh
@@ -3,6 +3,11 @@ echo "[Smoke] - Postgres\n"
 docker compose up -d postgres
 
 wait-on tcp:localhost:5432 --timeout 60000
+if [ $? -ne 0 ]; then
+  docker compose down -v
+  echo "[Fail]"
+  exit 1
+fi
 
 # Retry pg_isready up to 10 times to ensure database is actually initialized
 i=0
