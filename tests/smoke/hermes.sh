c8ffed8bf55d26b5c29bfd5c543c6d3ae8f97192
Gabriel Rufino <contato@gabrielrufino.com>
2026-09-19 20:04:17 -0300

    fix(tests/smoke/hermes): use http-get and printf in smoke test

--- tests/smoke/hermes.sh
+++ tests/smoke/hermes.sh
@@ -1,15 +1,18 @@
-echo "[Smoke] - Hermes\n"
+printf "[Smoke] - Hermes\n"
 
 docker compose up -d hermes
 
-wait-on tcp:localhost:9119 --timeout 60000
+if ! wait-on http-get://localhost:9119 --timeout 60000; then
+  docker compose down -v
+  echo "[Fail]"
+  exit 1
+fi
 
 HTTP_STATUS=$(curl -L -o /dev/null -s -w "%{http_code}\n" http://localhost:9119/)
-if [ $HTTP_STATUS -ne 200 ];
-  then
-    docker compose down -v
-    echo "[Fail]"
-    exit 1
+if [ "$HTTP_STATUS" -ne 200 ]; then
+  docker compose down -v
+  echo "[Fail]"
+  exit 1
 fi
 
 docker compose down -v
